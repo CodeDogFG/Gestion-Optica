3437e58293ecee6ce1e283485b9afd4ba2033aa8
SinnsykFG <143673271+SinnsykFG@users.noreply.github.com>
2025-08-11 23:33:06 -0400

    Creacion de repositorio

---
+++ src/test/java/aiep/TAP/GestionOptica/GestionOpticaApplicationTests.java
@@ -0,0 +1,13 @@
+package aiep.TAP.GestionOptica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionOpticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
